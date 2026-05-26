f155f7c8e26c2e8f5490c83d9a5fdd52c3b63005
AdamKatrenic <adamkatrenic@gmail.com>
2026-05-26 21:33:01 +0200

    feat: add security config and custom user details service

---
+++ src/main/java/sk/adamkatrenic/bankingapi/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package sk.adamkatrenic.bankingapi.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sk.adamkatrenic.bankingapi.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+}
